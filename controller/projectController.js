92f8d0c92f3cf455bb19fcba736a5880abff5ebd
mkashif9 <kashifsoftdeveloper@gmail.com>
2025-01-07 14:44:59 +0500

    feat(project): implement get, getById, patch, delete, update

--- controller/projectController.js
+++ controller/projectController.js
@@ -1,23 +1,93 @@
-const project = require('../db/models/project')
-const catchAsync = require('../catchAsync')
-const createProject =  catchAsync(async (req,res,next) =>{
-    const body = req.body
-    const userId = req.user.id;
-    const newProject = await project.create({
-        title:body.title,
-        productImage:body.productImage,
-        price:body.price,
-        shortDescription:body.shortDescription,
-        description:body.description,
-        productUrl:body.productUrl,
-        category:body.category,
-        tags:body.tags,
-        createdBy: userId
+const project = require("../db/models/project");
+const catchAsync = require("../catchAsync");
+const user = require("../db/models/user");
+const AppError = require("../appError");
 
-    })
-    return res.status(201).json({
+// Create Project
+const createProject = catchAsync(async (req, res, next) => {
+  const body = req.body;
+  const userId = req.user.id;
+  const newProject = await project.create({
+    title: body.title,
+    productImage: body.productImage,
+    price: body.price,
+    shortDescription: body.shortDescription,
+    description: body.description,
+    productUrl: body.productUrl,
+    category: body.category,
+    tags: body.tags,
+    createdBy: userId,
+  });
+  return res.status(201).json({
+    status: "success",
+    data: newProject,
+  });
+}); 
+
+// Get All Projects
+const getAllProjects = catchAsync(async (req, res, next) => {
+    const userId =req.user.id
+  const result = await project.findAll({include:user, where:{ createdBy:userId}});
+  return res.json({ status: "success", data: result });
+});
+
+// Get Projects by id 
+const getProjectById = catchAsync(async (req,res,next) =>{
+    const userId =req.user.id
+    const projectId = req.params.id;
+    const result = await  project.findOne({include:user,where:{id:projectId, createdBy:userId}});
+    if(!result){
+        return next( new AppError('Invalid project id',400))
+    }
+    return res.json({
         status:'success',
-        data: newProject,
+        data:result
     })
 })
-module.exports = {createProject}
+
+// Update project
+const updateProject = catchAsync(async (req,res,next) =>{
+    const userId =req.user.id
+    const projectId = req.params.id;
+    const body = req.body
+    const result = await  project.findOne({where:{id:projectId, createdBy:userId}});
+    if(!result){
+        return next( new AppError('Invalid project id',400))
+    }
+
+    result.title = body.title
+    result.productImage = body.productImage
+    result.price = body.price
+    result.shortDescription = body.shortDescription
+    result.description = body.description
+    result.productUrl = body.productUrl
+    result.category = body.category
+    result.tags = body.tags
+   
+    const updatedResult = await result.save();
+
+    return res.json({
+        status:'success',
+        data:updatedResult
+    })
+    
+})
+const deleteProject = catchAsync(async (req,res,next) =>{
+    const userId =req.user.id
+    const projectId = req.params.id;
+    const body = req.body
+    const result = await  project.findOne({where:{id:projectId, createdBy:userId}});
+    console.log("result"+result);
+    
+    if(!result){
+        return next( new AppError('Invalid project id',400))
+    }
+    await result.destroy();
+
+    return res.json({
+        status:'success',
+        message:'project deleted successfuly!'
+    })
+    
+})
+module.exports = { createProject,getAllProjects,getProjectById,updateProject,deleteProject };
--- routes/projectRoute.js
+++ routes/projectRoute.js
@@ -1,8 +1,9 @@
 const { authentication, restrictTo } = require('../controller/authController');
-const { createProject } = require('../controller/projectController');
+const { createProject, getAllProjects, getProjectById, updateProject, deleteProject } = require('../controller/projectController');
 
 const router =  require('express').Router();
 
-router.route('/').post(authentication,restrictTo('1'),createProject)
-
+router.route('/').post(authentication,restrictTo('1'),createProject).get(authentication,getAllProjects)
+router.route('/:id').get(authentication,getProjectById)
+.patch(authentication,updateProject).delete(authentication,deleteProject) 
 module.exports = router;
